4ea82d07e74c2f162f8cd8cf3fa5afbd9059cc96
sydorenkorv <99117460+sydorenkorv@users.noreply.github.com>
2022-12-04 13:42:53 +0000

    done 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+
+const cardsMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+
+
+function createGalleryMarkup(galleryItems) {
+
+   return galleryItems.map(({ preview, original, description }) => {
+     return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+     `;
+   })
+     .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+
+ });
